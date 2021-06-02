5f6e00a793f9bda04d9513b832bf02f6afea4e8b
nascono <aryhuh@gmail.com>
2021-06-03 01:01:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<script>
+var contractSource = "" + 
+    "contract test {\n" +
+    "   function multiply(uint a) returns(uint d) {\n" +
+    "       return a * 7;\n" +
+    "   }\n" +
+    "}\n";
+
+var contractCompiled = web3.eth.compile.solidity(contractSource);
+/*
+var code = contractCompiled.code;
+var abi = contractCompiled.info.abiDefinition;
+
+console.log(abi);
+var contract = web3.eth.contract(abi);
+contract.new(
+   {
+     from: web3.eth.accounts[0], 
+     data: code, 
+     gas: '4700000'
+   }, function (e, contract){
+      console.log(e, contract);
+      if (typeof contract.address !== 'undefined') {
+         console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+       }
+});*/
+
+
+
+</script>
+</html>
