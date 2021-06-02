3dd93264a1a2b970070dd224c8b15bedc30b0524
nascono <aryhuh@gmail.com>
2021-06-03 01:27:22 +0300

    asd

--- index.php
+++ index.php
@@ -1,35 +1,9 @@
-<html>
-<script>
+<?php
+use Web3\Web3;
+use Web3\Providers\HttpProvider;
+use Web3\RequestManagers\HttpRequestManager;
 
-var Contract = require('web3-eth-contract');
+$web3 = new Web3(new HttpProvider(new HttpRequestManager('http://localhost:8545')));
 
-/*var contractSource = "" + 
-    "contract test {\n" +
-    "   function multiply(uint a) returns(uint d) {\n" +
-    "       return a * 7;\n" +
-    "   }\n" +
-    "}\n";
-
-var contractCompiled = web3.eth.compile.solidity(contractSource);
-
-var code = contractCompiled.code;
-var abi = contractCompiled.info.abiDefinition;
-
-console.log(abi);
-var contract = web3.eth.contract(abi);
-contract.new(
-   {
-     from: web3.eth.accounts[0], 
-     data: code, 
-     gas: '4700000'
-   }, function (e, contract){
-      console.log(e, contract);
-      if (typeof contract.address !== 'undefined') {
-         console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-       }
-});*/
-
-
-
-</script>
-</html>
+print_r($web3);
+?>
