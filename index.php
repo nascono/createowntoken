0a97fdd1e92e74c44bf946b42665c8a1681f8ef6
nascono <aryhuh@gmail.com>
2021-06-04 18:03:58 +0300

    dasd

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-use Web3\Web3;
-use Web3\Providers\HttpProvider;
-use Web3\RequestManagers\HttpRequestManager;
-
-$web3 = new Web3(new HttpProvider(new HttpRequestManager('http://localhost:8545')));
-
-print_r($web3);
+include("page.html");
 ?>
